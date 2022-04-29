e7a796e69c4a5f99860e801d253c4e6dc0ecdb56
ShubhamBajaj <shmbajaj32@gmail.com>
2022-04-29 19:00:03 +0530

    feat: index file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Header, Footer } from "components";
+import { Home } from "pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "styles/common.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
