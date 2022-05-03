3b85d3cfe3310388d8681ee9459057215216872d
ShubhamBajaj <shmbajaj32@gmail.com>
2022-05-03 20:37:27 +0530

    Static data for readibility and understandability

---
+++ src/RouteSwitch.js
@@ -0,0 +1,35 @@
+import { VideoCard } from "components";
+import {
+  Home,
+  Auth,
+  Explore,
+  LikedVideos,
+  Playlists,
+  History,
+  WatchLater,
+  Videos,
+} from "pages";
+import { Routes, Route } from "react-router-dom";
+import Mockman from "mockman-js";
+
+function RouteSwitch() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Auth />} />
+      <Route path="/signup" element={<Auth />} />
+      <Route path="/videos" element={<Videos />}>
+        <Route index element={<Explore />} />
+        <Route path="explore" element={<Explore />} />
+        <Route path="liked" element={<LikedVideos />} />
+        <Route path="playlists" element={<Playlists />} />
+        <Route path="history" element={<History />} />
+        <Route path="later" element={<WatchLater />} />
+        <Route path="watch" element={<VideoCard />} />
+      </Route>
+      <Route path="/mockman" element={<Mockman />} />
+    </Routes>
+  );
+}
+
+export { RouteSwitch };
--- src/data.js
+++ src/data.js
@@ -8,13 +8,13 @@ const headerActionItems = [
     itemText: "Home",
     itemIconType: "home",
     isFlexDirectionColumn: true,
-    linkTo: "#",
+    linkTo: "/",
   },
   {
     itemText: "Explore",
     itemIconType: "explore",
     isFlexDirectionColumn: true,
-    linkTo: "#",
+    linkTo: "/videos/explore",
   },
   {
     itemText: "Login",
@@ -143,6 +143,120 @@ const signupFormFields = {
   linkTo: "/login",
 };
 
+const socialSkillsChannel = [
+  {
+    _id: "UCom1IdOkCHvzkOjbmck2gfg",
+    categoryName: "social skills",
+  },
+];
+
+const strengthChannel = [
+  {
+    _id: "UCftagEsyzmVwwKZyaFtNgBA",
+    categoryName: "strength",
+  },
+];
+
+const intelligenceChannel = [
+  {
+    _id: "UCFwsYHhZbudaV4No59q6mbQ",
+    categoryName: "intelligence",
+  },
+];
+
+const navActionItems = [
+  {
+    itemText: "Home",
+    itemIconType: "home",
+    isFlexDirectionColumn: false,
+    linkTo: "/",
+  },
+  {
+    itemText: "Explore",
+    itemIconType: "explore",
+    isFlexDirectionColumn: false,
+    linkTo: "/videos/explore",
+  },
+  {
+    itemText: "Liked Videos",
+    itemIconType: "thumb_up",
+    isFlexDirectionColumn: false,
+    linkTo: "/videos/liked",
+  },
+  {
+    itemText: "Playlists",
+    itemIconType: "video_library",
+    isFlexDirectionColumn: false,
+    linkTo: "/videos/playlists",
+  },
+  {
+    itemText: "History",
+    itemIconType: "history",
+    isFlexDirectionColumn: false,
+    linkTo: "/videos/history",
+  },
+  {
+    itemText: "Watch Later",
+    itemIconType: "watch_later",
+    isFlexDirectionColumn: false,
+    linkTo: "/videos/later",
+  },
+];
+
+const categories = ["All", "Intelligence", "Social Skills", "Strength"];
+
+const info = {
+  headingText: "",
+  showVideosCount: true,
+  videosCount: 0,
+  iconText: "open_in_new",
+};
+
+const watchLaterInfo = { ...info, headingText: "Watch later" };
+
+const likedVideosInfo = {
+  ...info,
+  headingText: "Liked Videos",
+};
+
+const playlistsInfo = {
+  ...info,
+  headingText: "All Playlists",
+  showVideosCount: false,
+};
+
+const historyInfo = {
+  ...info,
+  headingText: "History",
+};
+
+const videoCardFakeInfo = {
+  thumbnailPath:
+    "https://pbs.twimg.com/profile_images/1310736373247139840/hentCEA2_400x400.jpg",
+  videoId: "M7lc1UVf-VE",
+  title: `            Basics Episode 05: Startups VS Stable Jobs - Your Job is Not Secure
+  Basics Episode 05: Startups VS Stable Jobs - Your Job is Not Secure
+  Basics Episode 05: Startups VS Stable Jobs - Your Job is Not Secure
+  Basics Episode 05: Startups VS Stable Jobs - Your Job is Not Secure
+  Basics Episode 05: Startups VS Stable Jobs - Your Job is Not Secure`,
+  avatarPath:
+    "https://pbs.twimg.com/profile_images/1310736373247139840/hentCEA2_400x400.jpg",
+  creatorName: "Varun Mayya",
+  views: "531",
+  duration: "5:31",
+  description: `For a distraction free viewing experience, Download the Avalon
+  Meta App now: http://bit.ly/30mbRhG
+  ----------------------------------------------------------------------------------------------------
+  In this episode, we explore how automation is making 'Stable' jobs
+  a thing of the past, and why running a startup isn't as risky as
+  people make it out to be.
+  ----------------------------------------------------------------------------------------------------
+  Click here to subscribe to my YouTube channel:
+  https://www.youtube.com/channel/UCdEy...
+  ----------------------------------------------------------------------------------------------------
+  Instagram: https://www.instagram.com/thevarunmayya/`,
+};
+
 export {
   logoPath,
   appName,
@@ -151,4 +265,14 @@ export {
   categoriesCards,
   loginFormFields,
   signupFormFields,
+  socialSkillsChannel,
+  strengthChannel,
+  intelligenceChannel,
+  navActionItems,
+  categories,
+  watchLaterInfo,
+  likedVideosInfo,
+  playlistsInfo,
+  historyInfo,
+  videoCardFakeInfo,
 };
