5c085591b2447c70adabba74d2c37f8d273e7fd2
ShubhamBajaj <shmbajaj32@gmail.com>
2022-05-16 00:47:36 +0530

    Barell Exporting

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,3 +5,4 @@ export { useLikedVideosData } from "./useLikedVideosData/useLikedVideosData";
 export { useVideoData } from "./useVideoData/useVideoData";
 export { useWatchLaterData } from "./useWatchLaterData/useWatchLaterData";
 export { useHistoryData } from "./useHistoryData/useHistoryData";
+export { usePlaylistsData } from "./usePlaylistsData/usePlaylistsData";
